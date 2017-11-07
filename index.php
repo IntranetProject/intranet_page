33299ef02da10d37948b4c4fc0b0198bd03654c1
Tobi2K <tobika2000.tk@gmail.com>
2017-11-07 22:22:18 +0100

    title added, bothered me lol

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if (!(file_exists("up.txt"))) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+  <title>Intranetproject.net</title>
 </head>
 
 <body>
